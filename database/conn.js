fc57528eee0a47d632494f6a642450317bbaefbe
Evgeniy Rybin <z.ribin20@gmail.com>
2022-08-06 21:11:17 +0500

    Прикрепил БАЗУ

---
+++ database/conn.js
@@ -0,0 +1,24 @@
+const { MongoClient } = require("mongodb");
+
+let defaultMongoUri = "mongodb://mongodb:mongodb@mongodb:27017"
+const client = new MongoClient(process.env.MONGODB_URI || defaultMongoUri);
+
+let dbConnection;
+
+module.exports = {
+    connectToServer: function (callback) {
+        client.connect(function (err, db) {
+            if (err || !db) {
+                return callback(err);
+            }
+
+            dbConnection = db.db(process.env.MONGODB_DB_NAME || "quote_dev");
+
+            return callback();
+        });
+    },
+
+    getDb: function () {
+        return dbConnection;
+    },
+};
---
+++ routes/quotes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { getDb } = require('../database/conn')
+const router = express.Router();
+
+/* GET users listing. */
+router.get('/', async function (req, res) {
+  let db = getDb()
+  let quotes = await db.collection("quotes").find({}).toArray()
+
+  res.json(quotes)
+});
+
+module.exports = router;
