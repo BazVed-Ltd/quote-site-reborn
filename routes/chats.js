5a267ebb793f5526616454d8f455b880356146cb
Evgeniy Rybin <z.ribin20@gmail.com>
2022-08-09 19:03:44 +0500

    Добавил полей в возврат

--- routes/chats.js
+++ routes/chats.js
@@ -22,12 +22,35 @@ router.get('/:chatId', async function (req, res) {
   const db = getDb()
   const chatId = req.params.chatId
 
+  const chat = await db.collection('chats').findOne({id: +chatId})
+
+  console.log(chat)
+
   const quotes = await db.collection('quotes')
-    .find({ peer_id: +chatId === 0 ? null : +chatId })
+    .find({ peer_id: +chatId })
     .sort({ id: -1 })
     .toArray()
 
-  res.json(quotes)
+  const userIds = new Set()
+  function getUniqueUserIds(root) {
+    userIds.add(root.from_id)
+    for (const message of root.fwd_messages) {
+      userIds.add(message.from_id)
+      getUniqueUserIds(message)
+    }
+  }
+
+  quotes.forEach(quote => getUniqueUserIds(quote))
+
+  console.log(userIds)
+
+  const users = await db.collection('cache')
+    .find({ id: { $in: Array.from(userIds) } })
+    .toArray()
+
+  console.log(users)
+
+  res.json({ chat, quotes, users })
 })
 
 module.exports = router
